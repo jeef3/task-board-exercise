8c500bb599da9bdc7d1e0d52d6263aa550b2bbc4
Jeff Knaggs <mail@jeef3.com>
2024-09-28 16:07:44 +1200

    Remove import

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
