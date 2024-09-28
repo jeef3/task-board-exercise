1db6f35425e49f50f0e3d7d8aed14a931bb84fe8
Jeff Knaggs <mail@jeef3.com>
2024-09-28 16:50:19 +1200

    Testing types

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { gql, useQuery } from "@apollo/client";
+import type { Query } from "./types";
 
 const GET_ME = gql`
   query me {
@@ -35,14 +36,14 @@ const GET_ME = gql`
 `;
 
 export default function App() {
-  const { loading, error, data } = useQuery(GET_ME);
+  const { loading, error, data } = useQuery<Query>(GET_ME);
 
   if (loading) return <p>Loading…</p>;
   if (error) return <p>Error: {error.message}</p>;
 
   return (
     <>
-      <p className="read-the-docs">Hi</p>
+      <p className="read-the-docs">Hi, {data?.me.firstName}</p>
       <pre> {JSON.stringify(data, null, 2)}</pre>
     </>
   );
