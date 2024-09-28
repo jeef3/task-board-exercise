e95af5ae8b1eea160d7b4522ebef5052e4e9cd73
Jeff Knaggs <mail@jeef3.com>
2024-09-28 16:19:37 +1200

    Set up basic fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,50 @@
+import { gql, useQuery } from "@apollo/client";
+
+const GET_ME = gql`
+  query me {
+    me {
+      id
+      email
+      firstName
+      lastName
+
+      createdAt
+      updatedAt
+
+      memberships {
+        role {
+          id
+          __typename
+          ... on AdminRole {
+            admin
+          }
+          ... on UserRole {
+            admin
+            write
+          }
+        }
+
+        organisation {
+          id
+          name
+          timezone
+        }
+      }
+    }
+  }
+`;
+
 export default function App() {
+  const { loading, error, data } = useQuery(GET_ME);
+
+  if (loading) return <p>Loading…</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
   return (
     <>
-      <p className="read-the-docs">Hi </p>
+      <p className="read-the-docs">
+        Hi<pre> {JSON.stringify(data, null, 2)}</pre>
+      </p>
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import {
+  ApolloClient,
+  ApolloProvider,
+  createHttpLink,
+  InMemoryCache,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+
 import App from "./App.tsx";
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+
+const httpLink = createHttpLink({
+  uri: import.meta.env.VITE_APP_GRAPHQL_URI,
+});
+
+const authLink = setContext((_, { headers }) => ({
+  headers: {
+    ...headers,
+    Authorization: import.meta.env.VITE_APP_USER_ID,
+  },
+}));
 
 const client = new ApolloClient({
-  uri: import.meta.env.VITE_APP_GRAPHQL_URI,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
