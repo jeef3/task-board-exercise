ba412be6d88d5b62743f2f2303ef95716ad60f8a
Jeff Knaggs <mail@jeef3.com>
2024-10-15 16:13:02 +1300

    Move old logic out of App

--- src/hooks/CurrentBoardContext.tsx
+++ src/hooks/CurrentBoardContext.tsx
@@ -3,20 +3,34 @@ import {
   Dispatch,
   ReactNode,
   SetStateAction,
+  useEffect,
   useState,
 } from "react";
 
 import { Board } from "../__generated__/graphql";
+import { useCurrentOrg } from "./hooks";
 
 export const CurrentBoardContext = createContext<
   [current: Board | null, setCurrent: Dispatch<SetStateAction<Board | null>>]
 >([null, () => null]);
 
 export function CurrentBoardProvider({ children }: { children: ReactNode }) {
-  const current = useState<Board | null>(null);
+  const [currentBoard, setCurrentBoard] = useState<Board | null>(null);
+
+  const { data: { organisation } = {} } = useCurrentOrg();
+
+  useEffect(() => {
+    if (!organisation) return;
+
+    const firstBoard = organisation.boards[0];
+
+    if (!firstBoard) return;
+
+    setCurrentBoard(firstBoard as Board);
+  }, [organisation]);
 
   return (
-    <CurrentBoardContext.Provider value={current}>
+    <CurrentBoardContext.Provider value={[currentBoard, setCurrentBoard]}>
       {children}
     </CurrentBoardContext.Provider>
   );
